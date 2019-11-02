be33e35702a8544219aaedf8dd963315988e0216
Efosa Asemota <efosa.asemota@morehouse.edu>
2019-11-02 12:12:24 -0400

    new DB

---
+++ jobPortal.sql
@@ -0,0 +1,87 @@
+DROP DATABASE IF EXISTS jobPortal;
+CREATE DATABASE jobPortal;
+USE jobPortal;
+
+CREATE TABLE job_seeker (
+	seeker_id 		INT 			PRIMARY KEY      AUTO_INCREMENT 	NOT NULL ,
+    first_name		VARCHAR(25)								                     ,
+    last_name		VARCHAR(25)								                     ,
+    age				INT										                     ,
+    gender   		VARCHAR(20)												     ,
+    birthday    	DATE 														 ,
+    email_address 	VARCHAR(45)											 NOT NULL,
+    password 		VARCHAR(25)											 NOT NULL,
+    resume			VARCHAR(100)							
+);
+
+
+CREATE TABLE company (
+	company_id 				INT			PRIMARY KEY			AUTO_INCREMENT   	NOT NULL,
+    company_name			VARCHAR(45)	
+
+);
+
+CREATE TABLE recruiter (
+
+	employer_id		INT 			PRIMARY KEY		AUTO_INCREMENT		NOT NULL,
+    company_id		INT													NOT NULL,
+    first_name		VARCHAR(25)													,
+    last_name		VARCHAR(25)						     						,
+    email_address	VARCHAR(45)											NOT NULL,
+    password		VARCHAR(25)											NOT NULL,
+
+    
+    CONSTRAINT company_fk_recruiter
+		FOREIGN KEY (company_id)
+        REFERENCES company (company_id)
+
+);
+
+
+CREATE TABLE jobs (
+    job_id			INT 			 PRIMARY KEY 	AUTO_INCREMENT	 NOT NULL,
+    job_name		VARCHAR(45)                                			     ,
+    experience   	INT														 ,
+    job_status		VARCHAR(10)												 ,
+    application_due DATE   													 ,
+    street_address 	VARCHAR(100)											 ,
+    city 			VARCHAR(45)												 ,
+    state			VARCHAR(25)												 ,
+    job_description	VARCHAR(100)										     ,
+    company_id		INT												 NOT NULL,
+    salary			INT 													 ,
+    
+	CONSTRAINT company_fk_jobs
+		FOREIGN KEY (company_id)
+        REFERENCES company (company_id)
+		
+
+);
+
+CREATE TABLE applications (
+	application_id			INT 				AUTO_INCREMENT	PRIMARY KEY,
+    job_id	  				INT 										   ,
+    seeker_id				INT 										   ,
+	applied_date			DATE										   ,
+    
+    CONSTRAINT applications_fk_jobs
+		FOREIGN KEY (job_id)
+        REFERENCES jobs (job_id),
+        
+	CONSTRAINT applications_fk_job_seeker
+		FOREIGN KEY (seeker_id)
+        REFERENCES job_seeker (seeker_id)
+
+
+
+);
+
+
+
+
+
+
+
+
+
+
