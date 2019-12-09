26e70a6209c54413c9af3ed87d8c50336b7d7b93
Efosa Asemota <efosa.asemota@morehouse.edu>
2019-12-09 02:18:59 -0500

    Fixed minor things

--- returnJobs.php
+++ returnJobs.php
@@ -14,20 +14,33 @@ if(!empty($job_title)){
     //create connection
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
     
-    $SELECT = "SELECT job_id, job_title, company_id, salary FROM jobs WHERE job_title LIKE '".$job_title."'";
+    $SELECT = "SELECT job_id, job_title, city, company_id, salary FROM jobs WHERE job_title LIKE '".$job_title."'";
     $result = $conn->query($SELECT);
     
     if($result->num_rows > 0){
         
-        echo"<table>";
-        echo"<tr><td>ID</td><td>Job Title</td><td>Company #</td><td>Salary</td></tr>";
+        echo"<table width=700 border=5>";
+        echo"<tr><td>ID</td><td>Job Title</td><td>City</td><td>Company #</td><td>Salary</td><tr>";
         while($row = $result->fetch_assoc()){
-            echo "<tr><td>{$row['job_id']}</td><td>{$row['job_title']}</td><td>{$row['company_id']}</td><td>{$row['salary']}</td></tr>";
+            echo "<tr><td>{$row['job_id']}</td><td>{$row['job_title']}</td><td>{$row['city']}</td><td>{$row['company_id']}</td><td>{$row['salary']}</td><tr>";
         }
         echo"</table>";
+        
+        
+        
     } else {
         echo "No Results.";
     }
+    
+    $companyQuery = "SELECT * FROM company";
+    $companyResult = $conn->query($companyQuery);
+    
+    echo"<table width=700 border=2 padding-top=10";
+    echo"<tr><td>Company #</td><td>Company Name</td><tr>";
+    while($cRow = $companyResult->fetch_assoc()){
+        echo "<tr><td>{$cRow['company_id']}</td><td>{$cRow['company_name']}</td><tr>";
+    }
+    
     
     $conn->close();
     
